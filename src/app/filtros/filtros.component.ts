6cd3d1d59c00883035324eae7cf5950b1614350a
Laura-Ale <56073821+Laura-Ale@users.noreply.github.com>
2022-08-10 22:53:56 +0200

    Redirección botón filtros

--- src/app/filtros/filtros.component.ts
+++ src/app/filtros/filtros.component.ts
@@ -7,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FiltrosComponent implements OnInit {
 
-  constructor() { }
+  public boton: string;
+
+  constructor() { 
+    this.boton = "aplicar filtros";
+   }
 
   ngOnInit(): void {
   }
