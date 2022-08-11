3e1fdbcb65c7f2cf0e05cbbe344eb238573fb5fd
Laura-Ale <56073821+Laura-Ale@users.noreply.github.com>
2022-08-11 16:02:53 +0200

    All filters added

--- src/app/filtros/filtros.component.ts
+++ src/app/filtros/filtros.component.ts
@@ -7,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FiltrosComponent implements OnInit {
 
+  public titulo: string;
+  public filtros: Array<string>;
+  public genero: Array<string>;
+  public idioma: Array<string>;
+  public premio: Array<string>;
   public boton: string;
+  public pie: string;
 
   constructor() { 
+    this.titulo = "filtros";
+    this.filtros = ["genero", "duración", "actor/es", "director/es", "año", "puntuacion", "idioma", "premios"];
+    this.genero = ["romance", "terror", "drama", "fantasía", "+"];
+    this.idioma = ["inglés", "español", "turco", "+"];
+    this.premio = ["goya", "óscar", "+"];
     this.boton = "aplicar filtros";
+    this.pie = "Borrar filtros";
    }
 
   ngOnInit(): void {
